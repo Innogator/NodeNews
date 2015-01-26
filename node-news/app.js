794eb7544200f8638ca9db3b44cd7b536f40411a
Bryan Tanner <btanne6@yahoo.com>
2015-01-26 00:35:47 +0000

    Port problem on Cloud9, can't start app

--- node-news/app.js
+++ node-news/app.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+
+require('./models/Posts');
+require('./models/Comments');
+
+mongoose.connect('mongodb://localhost/news');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -27,6 +33,7 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+    console.log('error: ' + req);
     var err = new Error('Not Found');
     err.status = 404;
     next(err);
